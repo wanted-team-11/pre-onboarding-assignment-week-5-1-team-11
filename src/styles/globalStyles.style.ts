013a89be1505e636bf7233026bff50052645f196
gkdfo40 <gkdfo40@gmail.com>
2022-09-27 22:50:01 +0900

    feat: 스포카 한 산스 폰트 추가

--- src/styles/globalStyles.style.ts
+++ src/styles/globalStyles.style.ts
@@ -2,5 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
   ${reset}
+  font-family: 'Spoqa Han Sans Neo', 'sans-serif';
 `;
 export default GlobalStyle;
