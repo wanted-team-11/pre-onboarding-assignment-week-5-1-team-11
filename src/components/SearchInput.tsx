da9e86552fca8d90fcb59675b139e9bd50bdbe1a
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2022-09-28 14:11:00 +0900

    feat: SearchButton 추가

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,6 +4,7 @@ function Input() {
   return (
     <StyledInputWrapper>
       <StyledInput />
+      <StyledButton>검색</StyledButton>
     </StyledInputWrapper>
   );
 }
@@ -15,9 +16,21 @@ const StyledInputWrapper = styled.div``;
 const StyledInput = styled.input`
   border: 0;
   width: 400px;
-  height: 40px;
+  height: 44px;
   border-top-left-radius: 50px;
   border-bottom-left-radius: 50px;
   font-size: 20px;
   padding-left: 15px;
+`;
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  border: 0;
+  background-color: #539bf5;
+  width: 100px;
+  height: 47px;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
+  font-size: 18px;
+  color: #fff;
 `;
