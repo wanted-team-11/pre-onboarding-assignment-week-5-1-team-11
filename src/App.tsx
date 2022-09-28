bfa3da45c653445d6040863a881e7137efb4bdc4
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2022-09-28 13:17:11 +0900

    feat: Title 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Title from "./components/Title";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title>국내 모든 임상시험 검색하고 온라인으로 참여하기</Title>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+function Title({ children }: { children: string }) {
+  return <h2>{children}</h2>;
+}
+
+export default Title;
