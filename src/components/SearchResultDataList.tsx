cb4f93e301aa638cd5714185354c83183fc7172a
gkdfo40 <gkdfo40@gmail.com>
2022-09-28 11:44:01 +0900

    feat: 스타일 추가 & props 타입선언

--- src/components/SearchResultDataList.tsx
+++ src/components/SearchResultDataList.tsx
@@ -1,9 +1,48 @@
-const SearchResultDataList = () => {
+import styled from "styled-components";
+import GetFetchType from "../types/getFetchType";
+
+interface SearchResultDataListProps {
+  sickListVisible: boolean;
+  debouncedSearchInputValue: string;
+  fetchedSickList: GetFetchType[];
+}
+
+const SearchResultDataList = ({
+  sickListVisible,
+  debouncedSearchInputValue,
+  fetchedSickList,
+}: SearchResultDataListProps) => {
+  if (!sickListVisible) return null;
+  if (debouncedSearchInputValue.length === 0)
+    return (
+      <ListContainer>
+        <span>검색어 없음</span>
+      </ListContainer>
+    );
   return (
-    <ul>
-      <li>데이터</li>
-    </ul>
+    <ListContainer>
+      {fetchedSickList.map((sickItem) => (
+        <li key={sickItem.sickCd}>
+          <span>{sickItem.sickNm}</span>
+        </li>
+      ))}
+    </ListContainer>
   );
 };
 
 export default SearchResultDataList;
+
+const ListContainer = styled.ul`
+  width: 440px;
+  border-radius: 10px;
+  background-color: white;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  margin: 10px 0;
+  li {
+    margin: 7px 0;
+    :hover {
+      background-color: #f0f0f0;
+    }
+  }
+`;
