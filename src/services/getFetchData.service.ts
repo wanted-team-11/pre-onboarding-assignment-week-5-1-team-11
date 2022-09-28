4c37b9f119812b0204c5ffa28765f3fd608bafa7
gkdfo40 <gkdfo40@gmail.com>
2022-09-28 18:28:47 +0900

    fix: 로컬캐시 기능 추가 검색 결과 저장하여 setTimeOut 이전까지 검색 결과를 메모리에 저장

--- src/services/getFetchData.service.ts
+++ src/services/getFetchData.service.ts
@@ -1,11 +1,23 @@
-const getFetchData = async <T>(query: string): Promise<T> => {
+import GetFetchType from "../types/getFetchType";
+import cache from "../utils/localCache.util";
+
+const getFetchData = async (query: string) => {
+  if (cache[query]) {
+    console.log("캐시됨");
+    return cache[query] as GetFetchType[];
+  }
   const response = await fetch(`http://localhost:4000/sick?q=${query}`, {
     method: "GET",
   });
   if (!response.ok) {
     throw new Error(response.statusText);
   }
-  const data = response.json();
+  const data = (await response.json()) as GetFetchType[];
+  cache[query] = data;
+  setTimeout(() => {
+    delete cache[query];
+    console.log("캐시 삭제");
+  }, 60000);
   return data;
 };
 export default getFetchData;
